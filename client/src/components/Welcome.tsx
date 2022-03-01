01d5cbd25d5db1d1d81b1784f6c3f00fec055489
Yan Duarte <yan_asd@hotmail.com>
2022-03-01 02:17:01 -0300

    Add Transaction contract interaction in welcome page

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,19 +1,16 @@
-import { FC, InputHTMLAttributes, ChangeEvent } from 'react';
+import React, { FC, InputHTMLAttributes, ChangeEvent, useContext, MouseEventHandler } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
-import { SiEthereum } from 'react-icons/si';
-import { BsInfoCircle } from 'react-icons/bs';
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+import { TransactionContext } from "../context/TransactionContext";
 
 import { Loader } from "./";
-
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   handleChange: (e: ChangeEvent<HTMLInputElement>, name?: string) => void;
 }
 
-
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
-
-
 
 const Input: FC<InputProps> = ({ placeholder, name, type, value, handleChange }) => (
   <input
@@ -27,31 +24,37 @@ const Input: FC<InputProps> = ({ placeholder, name, type, value, handleChange })
 );
 
 const Welcome = () => {
-  const handleChange = () => {
-    // Todo create handleChange
-  }
+  const { connectWallet, currentAccount, handleChange, formData, sendTransaction } = useContext(TransactionContext);
+ 
+  
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const { addressTo, amount, keyword, message} = formData;
+    e.preventDefault();
 
-  const connectWallet = () => {
-    // Todo create connectWallet
-  }
-
-  const handleSubmit = () => {
-    // Todo create handleSubmit
-  }
+    if(!addressTo || !amount || !keyword || !message) return;
+    sendTransaction();
+  };
 
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start flex-col mf:mr-10">
-          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1"> Send Crypto <br /> across the world</h1>
-          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto</p>
-          <button
-            type="button"
-            onClick={connectWallet}
-            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-          >
-            <p className="text-white text-base font-semibold">Connect Wallet</p>
-          </button>
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+            {" "}
+            Send Crypto <br /> across the world
+          </h1>
+          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+            Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto
+          </p>
+          {!currentAccount && (
+            <button
+              type="button"
+              onClick={connectWallet}
+              className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+            >
+              <p className="text-white text-base font-semibold">Connect Wallet</p>
+            </button>
+          )}
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
@@ -73,12 +76,8 @@ const Welcome = () => {
                 <BsInfoCircle fontSize={17} color="#fff" />
               </div>
               <div>
-                <p className="text-white font-light text-sm">
-                  0xafdsafdsaf...sadfsad
-                </p>
-                <p className="text-white font-semibold text-lg mt-1">
-                  Ethereum
-                </p>
+                <p className="text-white font-light text-sm">{!!currentAccount && currentAccount}</p>
+                <p className="text-white font-semibold text-lg mt-1">Ethereum</p>
               </div>
             </div>
           </div>
@@ -89,20 +88,23 @@ const Welcome = () => {
             <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
             <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
-            <div className='h-[1px] w-full bg-gray-400 my-2' />
-            {false ? ( // TODO: Create loader state 
+            <div className="h-[1px] w-full bg-gray-400 my-2" />
+            {false ? ( // TODO: Create loader state
               <Loader />
             ) : (
-              <button type='button' onClick={handleSubmit} className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer">
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+              >
                 Send Now
               </button>
             )}
           </div>
-
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Welcome;
